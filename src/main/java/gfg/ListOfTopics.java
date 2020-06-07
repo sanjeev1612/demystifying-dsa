e3212902b40298da9babbe063b37780a489870a6
Sanjeev Ranjan <coolsanjeev1606@gmail.com>
2020-06-07 15:51:14 +0530

    GFG start

---
+++ src/main/java/gfg/ListOfTopics.java
@@ -0,0 +1,31 @@
+package gfg;
+
+public class ListOfTopics {
+    /* TOPICS               PROBLEMS SOLVED           STATUS           COMMENTS
+    * Introduction              0                   IN PROGRESS
+    * Mathematics
+    * Bit Magic
+    * Recursion
+    * Arrays
+    * Searching
+    * Sorting
+    * Matrix
+    * Hashing
+    * Strings
+    * Linked List
+    * Stack
+    * Queue
+    * Tree
+    * BST
+    * Heap
+    * Graph
+    * Greedy
+    * BackTracking
+    * Dynamic Programming
+    * Graph Advanced
+    * Trie
+    * Segment Tree
+    * Disjoint Sets
+    * */
+
+}
